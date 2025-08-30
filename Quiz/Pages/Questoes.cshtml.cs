f421a5272b37ecaab393a0991286b1bc586becb8
samuel <samuelconnde@gmail.com>
2025-08-30 18:04:38 -0300

    Subindo projeto com a página de questões criadas

---
+++ Quiz/Pages/Questoes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quiz.Pages
+{
+    public class QuestoesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
